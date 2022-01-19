d220718064ff7d211f059999f6395dcab0aa3be1
Deijux <juanchocruz200516@gmail.com>
2022-01-19 12:31:41 -0500

    arreglos ortográficos

--- js/sweetAlert.js
+++ js/sweetAlert.js
@@ -1,8 +1,9 @@
 Swal.fire({
     title: 'Adverencia',
-    text: 'No se busca insitar al odio ni formar algun tipo de pelea, todo es con fines de entretener.',
+    text: 'No se busca incitar al odio ni formar algun tipo de pelea, todo es con fines de entretenimiento.',
     icon: 'info',
     confirmButtonText: 'Entendido',
     backdrop: true,
     allowOutsideClick: false,
+    allowEnterKey: true,
 })
