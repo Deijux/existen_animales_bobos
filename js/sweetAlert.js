e4793b47463dd690676a91eb412869c4a4ac98a0
Deijux <juanchocruz200516@gmail.com>
2022-01-19 12:27:36 -0500

    Se agregó una alerta para el inicio de la pagina

---
+++ js/sweetAlert.js
@@ -0,0 +1,8 @@
+Swal.fire({
+    title: 'Adverencia',
+    text: 'No se busca insitar al odio ni formar algun tipo de pelea, todo es con fines de entretener.',
+    icon: 'info',
+    confirmButtonText: 'Entendido',
+    backdrop: true,
+    allowOutsideClick: false,
+})
